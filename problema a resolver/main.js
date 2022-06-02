317c4f7ed18038b0834b7e160b1924bf7e7f45fe
silena1 <88542064+silena1@users.noreply.github.com>
2022-06-02 10:01:22 -0300

    Add files via upload

---
+++ problema a resolver/main.js
@@ -0,0 +1,23 @@
+//=== ejercicio 1===
+let alumnos=["silena", "guillermina","julian","abigail", "milena"]
+//document.write (alumnos)
+//=== ejercicio 2 ===
+//document.write(alumnos[4])
+
+//=== ejercicio 3 ===
+let nuevoalumno = 2;
+alumnos[nuevoalumno]="salma"
+//document.write (alumnos)
+
+//=== ejercicio 4 ==
+alumnos.push("alberto")
+alert("ALBERTO se ha unido a ALUMNOS")
+
+//=== ejercicio 5 ===
+alumnos.pop ()
+
+//=== ejercicio 6 ===
+alumnos.unshift (alberto)
+
+//=== ejercicio 7===
+alumnos.shift (alberto)
