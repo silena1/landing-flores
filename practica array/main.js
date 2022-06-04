b8ff9b0c380ea9fc173a30a95ba405681f1baff3
silena1 <88542064+silena1@users.noreply.github.com>
2022-06-03 21:41:31 -0300

    Add files via upload

---
+++ practica array/main.js
@@ -0,0 +1,45 @@
+//=== ejercicio 1===
+let alumnos=["silena", "guillermina","julian","abigail", "milena"]
+// //document.write (alumnos)
+
+// //=== ejercicio 2 ===
+// //document.write(alumnos.length)
+// //document.write(alumnos[4])
+
+// //=== ejercicio 3 ===
+// let nuevoalumno = 2;
+// alumnos[nuevoalumno]="salma"
+// //document.write (alumnos)
+
+// //=== ejercicio 4 ==
+// alumnos.push("alberto")
+// alert("ALBERTO se ha unido a ALUMNOS")
+
+// //=== ejercicio 5 ===
+// alumnos.pop ()
+
+// //=== ejercicio 6 ===
+// alumnos.unshift (alberto)
+
+// //=== ejercicio 7===
+// alumnos.shift (alberto)
+
+// //=== ejercicio 8 ===
+//  let ingresantes2023=["maria","juan","pedro"]
+//  let nuevoaño = alumnos.concat(ingresantes2023)
+// //  document.write (nuevoaño)
+
+// // === ejercicio 9 ==
+// nuevoaño.sort()
+// document.write(nuevoaño)
+
+// // === ejercicio 10 ===
+// //while() {
+// let listasuper = ["harina","azucar","huevos","cacao","manteca",]
+// let nuevo = prompt("ingrese un nuevo elemento a la lista ")
+// let compra = listasuper.concat(nuevo)
+// console.log(listasuper.includes(nuevo))
+// if(listasuper.includes(nuevo)) {alert("este elemento ya pertenece a la lista")}
+// document.write (compra)
+// // }
+
