cdae51f4f0ca5acbea5a8341da6ef86570319b75
silena1 <88542064+silena1@users.noreply.github.com>
2022-06-22 14:58:01 -0300

    Add files via upload

---
+++ clases js/main.js
@@ -0,0 +1,17 @@
+class product{
+    constructor(name, price, stock) {
+        this.name= name;
+        this.price = price;
+        this.stock = stock;
+    }
+    stockproduct() {
+
+        this.stock--;
+
+    }
+}
+
+const box1 = new product('largebox', 20, 100);
+const box2 =new product('smallbox',15, 100);
+
+digital.write(box1)
