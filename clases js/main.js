933d29dff262951d15b1f8d7a4a38370c7b2521a
silena1 <88542064+silena1@users.noreply.github.com>
2022-06-23 08:17:19 -0300

    Update main.js

--- clases js/main.js
+++ clases js/main.js
@@ -4,14 +4,21 @@ class product{
         this.price = price;
         this.stock = stock;
     }
-    stockproduct() {
+    stockproduct(cantidad) {
 
-        this.stock--;
+       // this.stock--;
+       this.stock= this.stock-cantidad;
 
     }
+
+
 }
 
 const box1 = new product('largebox', 20, 100);
 const box2 =new product('smallbox',15, 100);
 
-console.log(box1)
+// box2.stockproduct() 
+// console.log (box2)
+
+box1.stockproduct(20)
+console.log (box1.stock);
