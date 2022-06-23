4993efeb05d18c15a8423ec1ed36ce0889d2902f
silena1 <88542064+silena1@users.noreply.github.com>
2022-06-23 09:32:20 -0300

    Update main.js

--- act/main.js
+++ act/main.js
@@ -5,3 +5,14 @@ const product =[
     {name:'motorola', price=500},
     {name:'xiaomi', price=800},
 ]
+
+//Recorre el array y busca el primer elemento cuyo precio es 600
+const iphone = products.find(product => product.price ===600);
+console.log (iphone);
+
+//Crea una copia del array anterior pero que contenga solo los precios de
+//los productos
+const newPrices = products.map (product =>{
+    return {name:product.name, price: product.price*1.500}
+});
+console.log (newPrices);
