86580e920f25d3768c4722aa828a34c51f695e41
silena1 <88542064+silena1@users.noreply.github.com>
2022-06-23 08:53:22 -0300

    Add files via upload

---
+++ act/main.js
@@ -0,0 +1,7 @@
+//array que contiene objetos
+const product =[
+    {name:'iphone', price=1000},
+    {name:'samsung', price=600},
+    {name:'motorola', price=500},
+    {name:'xiaomi', price=800},
+]
